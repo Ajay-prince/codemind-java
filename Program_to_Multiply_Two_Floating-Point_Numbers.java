4aae34ee9a0e093a9117f9bfdf2f510d3737dcc2
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-10-31 09:51:42 +0530

    import java.util.*; import java.lang.*; class Multiply{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         double a=sc.nextFloat();         double b=sc.nextFloat();         double c=a*b;         System.out.printf("%.2f",c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+class Multiply{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextFloat();
+        double b=sc.nextFloat();
+        double c=a*b;
+        System.out.printf("%.2f",c);
+    }
+}
