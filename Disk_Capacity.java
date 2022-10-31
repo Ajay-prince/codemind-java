4132ea4f9d8233bc2ac4b8f6f68a9ab61caa5ece
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-10-31 10:05:32 +0530

    import java.util.*; import java.lang.*; class Division{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         double d=2*a*b*c*512;         System.out.printf("%.0f",d);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+class Division{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        double d=2*a*b*c*512;
+        System.out.printf("%.0f",d);
+    }
+}
