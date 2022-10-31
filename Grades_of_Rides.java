deb33c1b75319cff33164199734948c9233d7762
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-10-31 16:19:00 +0530

    import java.lang.*; import java.util.*; class Check{     public static void main(String args[]){     Scanner sc=new Scanner(System.in);     int h=sc.nextInt();     int s=sc.nextInt();     int sp=sc.nextInt();     if(h>50&&s>60&&sp>100){         System.out.print(10);     }     else if(h>50&&s>60&&sp<100){         System.out.print(9);     }     else if(h<50&&s>60&&sp>100){         System.out.print(8);     }     else if(h>50&&s<60&&sp>100){         System.out.print(7);     }     else if(h>50||s>60||sp>100){         System.out.print(6);     }     else{         System.out.print(5);     }
    }
    }

---
+++ Grades_of_Rides.java
@@ -0,0 +1,29 @@
+import java.lang.*;
+import java.util.*;
+class Check{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int h=sc.nextInt();
+    int s=sc.nextInt();
+    int sp=sc.nextInt();
+    if(h>50&&s>60&&sp>100){
+        System.out.print(10);
+    }
+    else if(h>50&&s>60&&sp<100){
+        System.out.print(9);
+    }
+    else if(h<50&&s>60&&sp>100){
+        System.out.print(8);
+    }
+    else if(h>50&&s<60&&sp>100){
+        System.out.print(7);
+    }
+    else if(h>50||s>60||sp>100){
+        System.out.print(6);
+    }
+    else{
+        System.out.print(5);
+    }
+    
+}
+}
