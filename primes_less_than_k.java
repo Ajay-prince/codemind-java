12234ad01a7452b81c7cec4c090bc4b21247e97f
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-11-02 10:47:10 +0530

    import java.util.*; import java.lang.*; class Prime{ public static void main(String args[]){ Scanner sc=new Scanner(System.in); int n=sc.nextInt(); int a[]=new int[n]; for(int i=0;i<n;i++){ a[i]=sc.nextInt();
    }
    int count=0;
    int b=sc.nextInt();
    
    for(int i=0;i<n;i++){
    if (a[i]>1){
    boolean flag=false;
    double k=Math.sqrt(a[i])+1;
    int value=(int)k;
    for(int j=2;j<=value;j++){
    if(a[i]%j==0 && a[i]!=2){
        
        flag=true;
    break;
    }
    }
    if(!flag && a[i]<=b){
    count++;
    } 
    }
    }
    System.out.print(count);
    }
    }

---
+++ primes_less_than_k.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+class Prime{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++){
+a[i]=sc.nextInt();
+
+}
+int count=0;
+int b=sc.nextInt();
+
+for(int i=0;i<n;i++){
+if (a[i]>1){
+boolean flag=false;
+double k=Math.sqrt(a[i])+1;
+int value=(int)k;
+for(int j=2;j<=value;j++){
+if(a[i]%j==0 && a[i]!=2){
+    
+    flag=true;
+break;
+}
+}
+if(!flag && a[i]<=b){
+count++;
+} 
+}
+}
+System.out.print(count);
+}
+}
