6c6512d579bc27a07f09e5929c1cc4ccbac0b004
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-11-02 11:51:40 +0530

    import java.util.*; import java.lang.*; class Elements{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++){             a[i]=sc.nextInt();         }         double sum=0;         for(int j=0;j<n;j++){             sum+=a[j];         }         double avg=sum/n;         int k=(int)avg;         int count=0;         for(int u=0;u<n;u++){             if(a[u]<=k){                 count++;             }         }         System.out.print(count);     } }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+class Elements{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        double sum=0;
+        for(int j=0;j<n;j++){
+            sum+=a[j];
+        }
+        double avg=sum/n;
+        int k=(int)avg;
+        int count=0;
+        for(int u=0;u<n;u++){
+            if(a[u]<=k){
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+}
