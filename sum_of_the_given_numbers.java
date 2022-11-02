7867cacdd1a6b0d979141b8f32e2c3e46dc7a9c9
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-11-02 22:45:30 +0530

    import java.util.*; import java.lang.*; class Reverse{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++){             int k=sc.nextInt();             int u=sc.nextInt();             System.out.print(k+u+" ");         }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+class Reverse{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            int k=sc.nextInt();
+            int u=sc.nextInt();
+            System.out.print(k+u+"
+");
+        }
+    }
+}
