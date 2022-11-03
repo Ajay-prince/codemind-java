d5a61cb07b3699e185bc41fd09829c0b927b0958
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-11-03 11:15:21 +0530

    import java.util.*; import java.lang.*; class Prime{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int t=n;         int k=1;         for(int i=0;i<t;i++){
                for(int j=k;j<=n;j++){
                    System.out.print(j+" ");
                }
                k+=1;
                System.out.print("
    ");
            }
        }
    }

---
+++ Print_the_following_pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+class Prime{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int t=n;
+        int k=1;
+        for(int i=0;i<t;i++){
+            
+            for(int j=k;j<=n;j++){
+                System.out.print(j+" ");
+            }
+            k+=1;
+            System.out.print("
+");
+        }
+    }
+}
