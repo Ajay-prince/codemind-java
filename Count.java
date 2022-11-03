a4d7198fcb0694725c85abc609f8cbf257fbb40e
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-11-03 17:53:24 +0530

    import java.lang.*; import java.util.*; class Count{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[n];         int even=0;         int odd=0;         for(int i=0;i<n;i++){             a[i]=sc.nextInt();             if(a[i]%2==0){                 even++;             }             else{                 odd++;             }         }         System.out.print(even+" "+odd);     } }

---
+++ Count.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.util.*;
+class Count{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int even=0;
+        int odd=0;
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            if(a[i]%2==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+        System.out.print(even+" "+odd);
+    }
+}
