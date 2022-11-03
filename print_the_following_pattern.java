97944b52a0931378649e7fa9e02718c676a18074
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-11-03 16:58:51 +0530

    import java.util.*; import java.lang.*; class Pattern{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int t=n-1;         String alpha[]={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};         for(int i=(n-1);i>=0;i--){             for(int j=t;j>=0;--j){                 System.out.print(alpha[i]+" ");             }             System.out.print(" ");             t-=1;
            }
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,18 +1,19 @@
 import java.util.*;
 import java.lang.*;
-class Prime{
+class Pattern{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int t=n;
-        int k=1;
-        for(int i=0;i<t;i++){
-            for(int j=1;j<=k;j++){
-                System.out.print(j);
+        int t=n-1;
+        String alpha[]={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        for(int i=(n-1);i>=0;i--){
+            for(int j=t;j>=0;--j){
+                System.out.print(alpha[i]+" ");
             }
-            k+=1;
             System.out.print("
 ");
+            t-=1;
+            
         }
     }
 }
