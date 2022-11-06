7a23baf910b0b5b716d09139a974c53a48505ea2
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-11-06 14:06:22 +0530

    import java.util.*; import java.lang.*; class Prime{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);        String str=sc.next();        String s=str.toLowerCase();        System.out.print(s);
        }
    }

---
+++ To_Lower_Case.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+class Prime{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+       String str=sc.next();
+       String s=str.toLowerCase();
+       System.out.print(s);
+       
+    }
+}
