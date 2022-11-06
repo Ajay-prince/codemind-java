a23a30d1d1be8334fbcac74def4d95ba6832535d
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-11-06 13:14:24 +0530

    import java.util.*; import java.lang.*; class Prime{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);        StringBuffer sb=new StringBuffer(sc.nextLine());        System.out.println("Hello Technicalhub");        System.out.print(sb);
        }
    }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+class Prime{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+       StringBuffer sb=new StringBuffer(sc.nextLine());
+       System.out.println("Hello Technicalhub");
+       System.out.print(sb);
+       
+    }
+}
