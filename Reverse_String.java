ab4216e4a3165f16be72dbad6a2b9b33da14e08c
Ajay-prince <86159513+Ajay-prince@users.noreply.github.com>
2022-11-06 13:54:56 +0530

    import java.util.*; import java.lang.*; class Prime{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);        StringBuffer sb=new StringBuffer(sc.nextLine());
           StringBuffer s=sb.reverse();
           System.out.print(s);
           
        }
    }

---
+++ Reverse_String.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+class Prime{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+       StringBuffer sb=new StringBuffer(sc.nextLine());
+       
+       StringBuffer s=sb.reverse();
+       System.out.print(s);
+       
+    }
+}
